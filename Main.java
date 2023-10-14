b8f552e47ba52d46a4bd5de9a0ff251edb511442
Valentina Santiago Villegas <valentina.santiago@alumno.cch.unam.mx>
2023-10-13 18:32:23 -0600

    Cambios en las leyendas con el usuario L20-32

--- Main.java
+++ Main.java
@@ -17,6 +17,19 @@ public class Main {
       new Vuelo("Chicago", "Miami", "10:30 AM", "12:30pm")
     );
 
+    System.out.println("Bienvenido a su reservación de vuelo");
+    System.out.println("Ingrese sus datos personales");
+    System.out.print("Nombre: ");
+    String nombre = scanner.nextLine();
+    System.out.print("Apellido: ");
+    String apellido = scanner.nextLine();
+    Pasajero pasajero = new Pasajero(nombre, apellido);
+    System.out.println("Bienvenide " + pasajero.getNombre() + " " + pasajero.getApellido());
+
+    System.out.println("Ingrese los datos de su vuelo");
+    System.out.print("¿Su vuelo es internacional? (S/N): ");
+    String esInternacional = scanner.nextLine();
+
     System.out.print("Ingrese origen del vuelo: ");
     String origen = scanner.nextLine();
     System.out.print("Ingrese destino del vuelo: ");
@@ -25,10 +38,10 @@ public class Main {
     List<Vuelo> resultados = gestionVuelos.buscarVuelos(origen, destino);
     if (resultados.isEmpty()) {
       System.out.println(
-        "No se encontraron vuelos de " + origen + " a " + destino
+        "No tenemos vuelos disponibles de " + origen + " a " + destino + " disculpe las molestias."
       );
     } else {
-      System.out.println("Vuelos de " + origen + " a " + destino + ":");
+      System.out.println("Su vuelos de " + origen + " a " + destino + ":");
       for (Vuelo vuelo : resultados) {
         System.out.println("Hora de salida: " + vuelo.getHoraSalida());
       }
